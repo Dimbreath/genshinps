ff2ac5073d1c5ccb07153a946ed0fe98099c16e0
quinella <98860103+quinelladesuu@users.noreply.github.com>
2022-04-04 11:52:29 +0800

    add featrue spps

--- ps.py
+++ ps.py
@@ -1,5 +1,23 @@
-update cmd and field ids.exe
+# halo this quinlea, patch for more feat :uw:
+
+patchPS(update cmd and field ids.exe
+add reqrspnotify)
+
+vmprotect assembly
 
 when opened
 start private server
-give all characters
+load genshin
+add custom blk
+give all characters(c6, 10)
+add primogems
+inf money
+inf items
+r5 limited weapons
+add imposter music
+
+wait(9999999999999999999999999999999999999999999999999999999999)
+
+# vmportect asmemebly
+if else elif then : 
+  then if elseif elseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseifelseif
